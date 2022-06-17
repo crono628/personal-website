7134aa8c9a2131b3a215343b2de9bc7e9fbc9111
crono628 <84817352+crono628@users.noreply.github.com>
2022-06-17 12:54:35 -0400

    adding contact page. not wired up to firebase yet.

--- src/components/subcomponents/Contact.js
+++ src/components/subcomponents/Contact.js
@@ -1,40 +1,63 @@
-import { Card, CardContent } from '@mui/material';
+import { Button, styled, TextField } from '@mui/material';
 import { Box } from '@mui/system';
-import React, { forwardRef } from 'react';
+import React from 'react';
 
-const Contact = forwardRef((props, ref) => {
+const CustomTextField = styled(TextField)({
+  '& label.Mui-focused': {
+    color: 'white',
+  },
+  '& .MuiInput-underline:after': {
+    borderBottomColor: 'white',
+  },
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: 'white',
+    },
+    '&:hover fieldset': {
+      borderColor: 'white',
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: 'white',
+    },
+  },
+});
+
+const Contact = () => {
   return (
-    <Card
+    <Box
       sx={{
         backgroundColor: '#4f83cc',
         width: '75vw',
         maxWidth: '650px',
         display: 'flex',
         flexDirection: 'column',
+        p: 2,
       }}
-      {...props}
-      ref={ref}
     >
-      <CardContent>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
-        reprehenderit nesciunt ut laudantium assumenda modi harum vero laborum
-        ipsum? Expedita corporis ex laborum incidunt architecto, vero maiores
-        unde nemo soluta.
-      </CardContent>
-      <CardContent>
-        Repellat tenetur neque voluptatibus nobis quae ea blanditiis iusto,
-        distinctio, reprehenderit unde ratione quod voluptates quaerat
-        repudiandae aspernatur nihil? Delectus officiis rem molestiae maiores
-        voluptatum illo error nihil odio. Voluptatem?
-      </CardContent>
-      <CardContent>
-        Ducimus, doloribus corrupti culpa tenetur quidem, rerum fuga blanditiis
-        ab reprehenderit provident explicabo aut amet, illo minima dolorum eum!
-        Nemo dolor quaerat fuga quam fugit exercitationem accusamus deleniti,
-        illo autem?
-      </CardContent>
-    </Card>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+          height: '350px',
+          mt: 2,
+          p: 2,
+        }}
+        component="form"
+      >
+        <CustomTextField label="Name" required />
+        <CustomTextField label="Email" required />
+        <CustomTextField
+          label="Message"
+          required
+          multiline
+          minRows={4}
+          maxRows={4}
+        />
+        <Button sx={{ color: 'white' }}>Submit</Button>
+      </Box>
+    </Box>
   );
-});
+};
 
 export default Contact;
--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyBg856JYoffxsnjw1q8zId8v1usTeNJ2K0',
-  authDomain: 'portfolio-70217.firebaseapp.com',
-  projectId: 'portfolio-70217',
-  storageBucket: 'portfolio-70217.appspot.com',
-  messagingSenderId: '1087722284949',
-  appId: '1:1087722284949:web:66a492e74a2c9e755df89c',
-  measurementId: 'G-M8P9FCZB4F',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 const analytics = getAnalytics(app);
+
+export { app, db, analytics };
