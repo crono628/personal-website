ebaab17a6c205f1f440af839165ecb7650e4b37c
crono628 <84817352+crono628@users.noreply.github.com>
2022-08-23 21:23:10 -0400

    added browser router and firebase. experimenting with design

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Welcome from './components/Welcome';
 
 const App = () => {
-  return <div>App</div>;
+  const [loading, setLoading] = useState(true);
+  return (
+    <div className="max-w-2xl bg-yellow-400 mx-auto border-violet-500 border-4">
+      <Welcome />
+    </div>
+  );
 };
 
 export default App;
