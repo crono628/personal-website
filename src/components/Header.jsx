b0bc664913e1751a6560e8562ae4d9cd1870ed86
crono628 <84817352+crono628@users.noreply.github.com>
2022-09-02 20:14:59 -0400

    scroll animations!

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,10 @@ import github from '../assets/github.svg';
 
 const Header = () => {
   return (
-    <div className="flex  p-3 w-full relative items-center">
-      <img src={github} className="w-6 sm:w-8  cursor-pointer absolute" />
-      <div className="silkscreen flex flex-1 flex-col ml-3 justify-center items-center text-lg sm:text-2xl w-full mx-auto">
-        <div>Michael DeSantis</div>
-        <div>Web Developer</div>
-      </div>
+    <div className="silkscreen flex flex-col ml-3 p-5 justify-center items-start text-lg sm:text-2xl w-full mx-auto">
+      <div>Michael DeSantis</div>
+      <div>Web Developer</div>
+      <div>--</div>
     </div>
   );
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,25 +1,73 @@
-import React from 'react';
+import React, { useEffect, useLayoutEffect, useRef } from 'react';
 
 const Home = () => {
-  return (
-    <div className="silkscreen flex flex-col mt-10 mx-auto max-w-lg px-10">
-      <p className="my-4  flex text-center">
-        Eloquent solutions to unique problems fuel my interest to learn more.
-      </p>
+  const scrollRef = useRef([]);
 
-      {/* <p className="my-4">
-        Leveling up my web development skills gives me an enormous sense of
-        fulfillment and pride. It's the keyboard and me, just a few keystrokes
-        away from a solution.
-      </p>
-      <p className="my-4">
-        Getting to intimately know every inch of a subject is my superpower;
-        whether it's coding, cooking, coffee, shaving, health, hiking, video
-        games or anything else I invest myself in, I always value an opportunity
-        to get closer to achieving mastery.
-      </p> */}
+  useLayoutEffect(() => {
+    window.addEventListener('scroll', handleAnimationScroll);
+
+    return () => window.removeEventListener('scroll', handleAnimationScroll);
+  }, []);
+
+  const handleAnimationScroll = () => {
+    console.log('window', window.offsetHeight);
+
+    scrollRef.current.forEach((ref, i) => {
+      if (
+        ref.getBoundingClientRect().top <
+        window.innerHeight - window.innerHeight / 3
+      ) {
+        ref.classList.add('left-slide');
+        ref.classList.remove('right-slide');
+      } else if (
+        ref.getBoundingClientRect().top >
+        window.innerHeight - window.innerHeight / 3
+      ) {
+        ref.classList.add('right-slide');
+        ref.classList.remove('left-slide');
+      }
+    });
+  };
+
+  // const { current } = scrollRef;
+  // const { top } = current.getBoundingClientRect();
+  // const isVisible = top > 0 && top < window.innerHeight;
+  // if (isVisible) {
+  //   current.classList.add('left-slide');
+  // }
+  // console.log('scroll');
+
+  return (
+    <div
+      onScroll={() => handleScrollAnimation(scrollRef[index])}
+      className="silkscreen flex flex-col items-start justify-start mt-10  max-w-lg px-10"
+    >
+      {homeText.map((text, index) => {
+        return (
+          <p
+            key={index}
+            ref={(el) => {
+              scrollRef.current[index] = el;
+            }}
+            className="flex flex-col items-start justify-start my-24"
+          >
+            {text}
+          </p>
+        );
+      })}
     </div>
   );
 };
 
 export default Home;
+
+const homeText = [
+  'Eloquent solutions to unique problems fuel my interest to learn more.',
+  "Leveling up my web development skills gives me an enormous sense of fulfillment and pride. It's the keyboard and me, just a few keystrokes away from a solution.",
+  "Getting to intimately know every inch of a subject is my superpower; whether it's coding, cooking, coffee, shaving, health, hiking, video games or anything else I invest myself in, I always value an opportunity to get closer to achieving mastery.",
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis sunt libero eius molestias, totam et ea tempore assumenda nobis ducimus cumque ipsum beatae ullam aliquid iste, ipsam sapiente in perferendis. Inventore eaque vero, repellat iusto ipsum temporibus illo velit, quidem repellendus aliquam odio autem, iure dolor veritatis tempora.',
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis sunt libero eius molestias, totam et ea tempore assumenda nobis ducimus cumque ipsum beatae ullam aliquid iste, ipsam sapiente in perferendis. Inventore eaque vero, repellat iusto ipsum temporibus illo velit, quidem repellendus aliquam odio autem, iure dolor veritatis tempora.',
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis sunt libero eius molestias, totam et ea tempore assumenda nobis ducimus cumque ipsum beatae ullam aliquid iste, ipsam sapiente in perferendis. Inventore eaque vero, repellat iusto ipsum temporibus illo velit, quidem repellendus aliquam odio autem, iure dolor veritatis tempora.',
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis sunt libero eius molestias, totam et ea tempore assumenda nobis ducimus cumque ipsum beatae ullam aliquid iste, ipsam sapiente in perferendis. Inventore eaque vero, repellat iusto ipsum temporibus illo velit, quidem repellendus aliquam odio autem, iure dolor veritatis tempora.',
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis sunt libero eius molestias, totam et ea tempore assumenda nobis ducimus cumque ipsum beatae ullam aliquid iste, ipsam sapiente in perferendis. Inventore eaque vero, repellat iusto ipsum temporibus illo velit, quidem repellendus aliquam odio autem, iure dolor veritatis tempora.',
+];
