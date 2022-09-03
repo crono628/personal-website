e86d94c2ab49b846eaebe46008065a3ca0c86032
crono628 <84817352+crono628@users.noreply.github.com>
2022-09-02 21:47:25 -0400

    fine tuning scroll animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,12 @@ import github from '../assets/github.svg';
 
 const Header = () => {
   return (
-    <div className="silkscreen flex flex-col ml-3 p-5 justify-center items-start text-lg sm:text-2xl w-full mx-auto">
-      <div>Michael DeSantis</div>
-      <div>Web Developer</div>
-      <div>--</div>
+    <div className=" silkscreen  ml-3 p-5 mb-32 text-lg sm:text-2xl w-full ">
+      <div className="fixed top-0 left-0 p-5 bg-orange-300 z-50 w-full">
+        <div>Michael DeSantis</div>
+        <div>Web Developer</div>
+        <div>--</div>
+      </div>
     </div>
   );
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,32 +10,17 @@ const Home = () => {
   }, []);
 
   const handleAnimationScroll = () => {
-    console.log('window', window.offsetHeight);
-
-    scrollRef.current.forEach((ref, i) => {
+    scrollRef.current.forEach((ref) => {
       if (
-        ref.getBoundingClientRect().top <
-        window.innerHeight - window.innerHeight / 3
+        ref.getBoundingClientRect().bottom <
+        window.innerHeight + window.innerHeight / 3
       ) {
-        ref.classList.add('left-slide');
-        ref.classList.remove('right-slide');
-      } else if (
-        ref.getBoundingClientRect().top >
-        window.innerHeight - window.innerHeight / 3
-      ) {
-        ref.classList.add('right-slide');
-        ref.classList.remove('left-slide');
+        ref.classList.add('slide-in');
+      } else if (ref.getBoundingClientRect().bottom > window.innerHeight) {
+        ref.classList.remove('slide-in');
       }
     });
   };
-
-  // const { current } = scrollRef;
-  // const { top } = current.getBoundingClientRect();
-  // const isVisible = top > 0 && top < window.innerHeight;
-  // if (isVisible) {
-  //   current.classList.add('left-slide');
-  // }
-  // console.log('scroll');
 
   return (
     <div
@@ -44,15 +29,15 @@ const Home = () => {
     >
       {homeText.map((text, index) => {
         return (
-          <p
+          <div
             key={index}
             ref={(el) => {
               scrollRef.current[index] = el;
             }}
-            className="flex flex-col items-start justify-start my-24"
+            className="hide flex flex-col items-start justify-start my-36"
           >
             {text}
-          </p>
+          </div>
         );
       })}
     </div>
@@ -65,6 +50,9 @@ const homeText = [
   'Eloquent solutions to unique problems fuel my interest to learn more.',
   "Leveling up my web development skills gives me an enormous sense of fulfillment and pride. It's the keyboard and me, just a few keystrokes away from a solution.",
   "Getting to intimately know every inch of a subject is my superpower; whether it's coding, cooking, coffee, shaving, health, hiking, video games or anything else I invest myself in, I always value an opportunity to get closer to achieving mastery.",
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis sunt libero eius molestias, totam et ea tempore assumenda nobis ducimus cumque ipsum beatae ullam aliquid iste, ipsam sapiente in perferendis. Inventore eaque vero, repellat iusto ipsum temporibus illo velit, quidem repellendus aliquam odio autem, iure dolor veritatis tempora.',
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis sunt libero eius molestias, totam et ea tempore assumenda nobis ducimus cumque ipsum beatae ullam aliquid iste, ipsam sapiente in perferendis. Inventore eaque vero, repellat iusto ipsum temporibus illo velit, quidem repellendus aliquam odio autem, iure dolor veritatis tempora.',
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis sunt libero eius molestias, totam et ea tempore assumenda nobis ducimus cumque ipsum beatae ullam aliquid iste, ipsam sapiente in perferendis. Inventore eaque vero, repellat iusto ipsum temporibus illo velit, quidem repellendus aliquam odio autem, iure dolor veritatis tempora.',
   'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis sunt libero eius molestias, totam et ea tempore assumenda nobis ducimus cumque ipsum beatae ullam aliquid iste, ipsam sapiente in perferendis. Inventore eaque vero, repellat iusto ipsum temporibus illo velit, quidem repellendus aliquam odio autem, iure dolor veritatis tempora.',
   'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis sunt libero eius molestias, totam et ea tempore assumenda nobis ducimus cumque ipsum beatae ullam aliquid iste, ipsam sapiente in perferendis. Inventore eaque vero, repellat iusto ipsum temporibus illo velit, quidem repellendus aliquam odio autem, iure dolor veritatis tempora.',
   'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis sunt libero eius molestias, totam et ea tempore assumenda nobis ducimus cumque ipsum beatae ullam aliquid iste, ipsam sapiente in perferendis. Inventore eaque vero, repellat iusto ipsum temporibus illo velit, quidem repellendus aliquam odio autem, iure dolor veritatis tempora.',
