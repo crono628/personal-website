3b87bafc93b10a5f7fe05a1c90eda99b9e4ebf5b
crono628 <84817352+crono628@users.noreply.github.com>
2023-07-18 17:49:24 -0400

    fresh start

--- src/App.tsx
+++ src/App.tsx
@@ -1,92 +1,34 @@
-import React, { useEffect, useRef, useState } from 'react'
-import About from './components/About'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
-import Header from './components/Header'
-import Projects from './components/Projects'
-import Resume from './components/Resume'
-import ScrollButton from './components/ScrollButton'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const App = () => {
-  const scrollRef = useRef([])
-  const [highlight, setHighlight] = useState([
-    { about: true, projects: false, resume: false, contact: false }
-  ])
-
-  useEffect(() => {
-    window.addEventListener('scroll', setHighlightState)
-    return () => window.removeEventListener('scroll', setHighlightState)
-  }, [])
-
-  function handleHighlightMath(ref) {
-    if (
-      ref.getBoundingClientRect().bottom + window.innerHeight / 1.5 >
-      window.innerHeight
-    ) {
-      return true
-    }
-    return false
-  }
-
-  const setHighlightState = () => {
-    const defaultValues = {
-      about: false,
-      projects: false,
-      resume: false,
-      contact: false
-    }
-
-    const aboutState = handleHighlightMath(scrollRef.current[0])
-    const projectsState = handleHighlightMath(scrollRef.current[1])
-    const resumeState = handleHighlightMath(scrollRef.current[2])
-    const contactState = handleHighlightMath(scrollRef.current[3])
-
-    if (aboutState) {
-      setHighlight([{ ...defaultValues, about: true }])
-    } else if (projectsState) {
-      setHighlight([{ ...defaultValues, projects: true }])
-    } else if (resumeState) {
-      setHighlight([{ ...defaultValues, resume: true }])
-    } else if (contactState) {
-      setHighlight([{ ...defaultValues, contact: true }])
-    } else {
-      setHighlight([{ ...defaultValues }])
-    }
-  }
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <div className="max-w-screen-md mx-auto flex flex-col justify-center items-center ">
-      <div className="w-full mx-auto bg-orange-300 min-h-screen relative">
-        <Header highlight={highlight} ref={scrollRef} />
-        <div className={`px-5  flex flex-col items-center `}>
-          <div className=" sm:max-w-sm flex flex-col items-center">
-            <div
-              className="h-[800px]"
-              ref={(el) => (scrollRef.current[0] = el)}
-            >
-              <About />
-            </div>
-            <div
-              className="h-[2400px]"
-              ref={(el) => (scrollRef.current[1] = el)}
-            >
-              <Projects />
-            </div>
-            <div
-              className="h-[1000px]"
-              ref={(el) => (scrollRef.current[2] = el)}
-            >
-              <Resume />
-            </div>
-            <div ref={(el) => (scrollRef.current[3] = el)}>
-              <Contact />
-            </div>
-          </div>
-          <ScrollButton below={200} />
-        </div>
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-      <Footer />
-    </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
